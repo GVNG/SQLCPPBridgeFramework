9caceee7d377cb213ae874092ad8c1eecce338c7
Roman Makhnenko <gvng@dataart.com>
2018-12-31 11:55:28 +0100

    Bones for the UnitTests

---
+++ UnitTests/src/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Tests
+//
+//  Created by Roman Makhnenko on 31/12/2018.
+//  Copyright © 2018 DataArt. All rights reserved.
+//
+
+#include "gtest/gtest.h"
+
+int main(int argc, char ** argv)
+{
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
