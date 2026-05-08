26e7c356fcccddc853d4af29f75aa35b4a91d312
GVNG <gvng@me.com>
2026-05-08 10:32:48 +0200

    Added SPM support

---
+++ Package.swift
@@ -0,0 +1,35 @@
+// swift-tools-version:5.9
+
+import PackageDescription
+
+let package = Package(
+    name: "SQLCppBridge",
+    platforms: [
+        .iOS(.v13),
+        .macOS(.v11),
+        .watchOS(.v6)
+    ],
+    products: [
+        .library(
+            name: "SQLCppBridge",
+            targets: ["SQLCppBridge"]
+        )
+    ],
+    targets: [
+        .target(
+            name: "SQLCppBridge",
+            path: "SQLBridge",
+            sources: [
+                "src"
+            ],
+            publicHeadersPath: "include",
+            cxxSettings: [
+                .headerSearchPath("include")
+            ],
+            linkerSettings: [
+                .linkedLibrary("sqlite3")
+            ]
+        )
+    ],
+    cxxLanguageStandard: .cxx14
+)
